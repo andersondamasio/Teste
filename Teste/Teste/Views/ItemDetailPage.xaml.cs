e25b31c88fd3371a56c1ce7ab14e557aa2cdcd3a
Anderson <Anderson@DESKTOP-6MK93JV>
2021-08-17 17:43:57 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/Teste/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Teste.ViewModels;
+using Xamarin.Forms;
+
+namespace Teste.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
